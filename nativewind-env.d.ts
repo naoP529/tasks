60eef3ee7382324c40e927ac46afc847c37354fb
naoP529 <nk02kjm3805@gmail.com>
2026-06-12 16:43:45 +0900

    add nativewind to the project

---
+++ nativewind-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="nativewind/types" />
+
+declare module '*.css';
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
